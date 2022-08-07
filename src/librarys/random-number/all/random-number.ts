9412e36a9e9a6e0c40b2db24a5b6bb09abc75ae6
wisdomstar94 <wisdomstar94@gmail.com>
2022-08-07 20:05:48 +0900

    범위를 분산하여 랜덤 숫자를 얻을 수 있는 함수 추가.

--- src/librarys/random-number/all/random-number.ts
+++ src/librarys/random-number/all/random-number.ts
@@ -1,3 +1,9 @@
 export const getRandomNumber = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const getRandomNumberFromRange = (range: {min: number, max: number}[]): number => {
+  const random = getRandomNumber(0, range.length - 1);
+  const randomValues = range.map(x => getRandomNumber(x.min, x.max));
+  return randomValues[random];
+};
